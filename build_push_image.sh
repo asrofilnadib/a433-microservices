9e7dbd0054cd2b4fb80011668b4beee6bc3bda0f
Asrofil Nadib <asrofilnadibs28@gmail.com>
2023-11-03 14:37:28 +0700

    change build_push_image.sh to add log.txt

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+echo "Image telah dibuild dan diberi tag pada $(date)" >> log.txt
+
 echo "Membuat docker images..."
 # Perintah untuk membuat Docker image dari Dockerfile
 docker build -t item-app:v1 .
 
-echo "\nList docker images"
+echo "List docker images"
 # Melihat daftar image di lokal
 docker images
 
-echo "\nMengubah nama sesuai dengan format docker registry"
+echo "Mengubah nama sesuai dengan format docker registry"
 # Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages)
 docker tag item-app:v1 ghcr.io/asrofilnadib/item-app:v1
 
-echo "\nLogin ke Github Packages"
+echo "Login ke Github Packages"
 # Login ke Docker Hub (atau GitHub Packages)
 echo "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" | docker login ghcr.io -u asrofilnadib --password-stdin
 
-echo "\nMengunggah image ke Github Packages"
+echo "Mengunggah image ke Github Packages"
 # Mengunggah image ke Docker Hub (atau GitHub Packages)
 docker push ghcr.io/asrofilnadib/item-app:v1
+
+echo "Image telah diunggah pada $(date)" >> log.txt
